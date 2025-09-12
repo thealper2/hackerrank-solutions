318d820d9969bb5a064353575222053a2f5bcecd
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-09-12 16:43:45 +0300

    Connecting Towns

---
+++ Mathematics/Fundamentals/connecting_towns.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'connectingTowns' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY routes
+#
+
+def connectingTowns(n, routes):
+    total = 1
+    MOD = 1234567
+    for num in routes:
+        total = (total * num) % MOD
+        
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        routes = list(map(int, input().rstrip().split()))
+
+        result = connectingTowns(n, routes)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
