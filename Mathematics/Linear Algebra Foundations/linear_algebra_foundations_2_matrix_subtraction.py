90700aa32ecb93323896ee3e0c65b34194b3a0a7
Alper Karaca <thisiseternity12@gmail.com>
2025-09-12 23:01:05 +0300

    Linear Algebra Foundations #2 - Matrix Subtraction

---
+++ Mathematics/Linear Algebra Foundations/linear_algebra_foundations_2_matrix_subtraction.py
@@ -0,0 +1,23 @@
+matrix1 = [
+    [1, 2, 3],
+    [2, 3, 4],
+    [1, 1, 1]
+]
+
+matrix2 = [
+    [4, 5, 6],
+    [7, 8, 9],
+    [4, 5, 0]
+]
+
+result = []
+for i in range(3):
+    row = []
+    for j in range(3):
+        row.append(matrix1[i][j] - matrix2[i][j])
+        
+    result.append(row)
+    
+for i in range(3):
+    for j in range(3):
+        print(result[i][j])
