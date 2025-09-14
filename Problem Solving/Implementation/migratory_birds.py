d64691883b86c30b4f678ebfa562ec23b2a198aa
Alper Karaca <thisiseternity12@gmail.com>
2025-09-14 13:51:50 +0300

    Migratory Birds

---
+++ Problem Solving/Implementation/migratory_birds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    freq = [0] * 5
+    for i in arr:
+        freq[i - 1] += 1
+        
+    max_freq = max(freq)
+    for i in range(5):
+        if freq[i] == max_freq:
+            return i + 1
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
