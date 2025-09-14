ef8143de9e7bb63b4bdcc66aa802a1479fbe7f6b
Alper Karaca <thisiseternity12@gmail.com>
2025-09-14 21:15:07 +0300

    Jumping on the Clouds: Revisited

---
+++ Problem Solving/Implementation/jumping_on_the_clouds_revisited.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    n = len(c)
+    energy = 100
+    current = 0
+    while True:
+        current = (current + k) % n
+        energy -= 1
+        if c[current] == 1:
+            energy -= 2
+        
+        if current == 0:
+            break
+            
+    return energy
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
