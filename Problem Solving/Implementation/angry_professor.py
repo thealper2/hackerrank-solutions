b026e87ef5894471bf640dd989a89c38ed36b930
Alper Karaca <thisiseternity12@gmail.com>
2025-09-14 18:16:18 +0300

    Angry Professor

---
+++ Problem Solving/Implementation/angry_professor.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a):
+    on_time = sum(1 for t in a if t <= 0)
+    return "NO" if on_time >= k else "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
