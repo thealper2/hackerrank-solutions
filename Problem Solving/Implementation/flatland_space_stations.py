99bc1a342e7653576789cc013e4e8a027be2b580
Alper Karaca <thisiseternity12@gmail.com>
2025-09-14 23:02:33 +0300

    Flatland Space Stations

---
+++ Problem Solving/Implementation/flatland_space_stations.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the flatlandSpaceStations function below.
+def flatlandSpaceStations(n, c):
+    c.sort()
+    max_dist = max(c[0], n - 1 - c[-1])
+    for i in range(1, len(c)):
+        max_dist = max(max_dist, (c[i] - c[i - 1]) // 2)
+        
+    return max_dist
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = flatlandSpaceStations(n, c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
