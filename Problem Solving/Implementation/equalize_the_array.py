6cd77d3e7f6cfa9f34c5dbc3f90980387ab312f0
Alper Karaca <thisiseternity12@gmail.com>
2025-09-14 22:09:02 +0300

    Equalize the Array

---
+++ Problem Solving/Implementation/equalize_the_array.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    freq = Counter(arr)
+    max_freq = max(freq.values())
+    return len(arr) - max_freq
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
