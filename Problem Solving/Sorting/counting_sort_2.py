936cb6361d3c8a2b7fde17113d1d0c45caf9c3cb
Alper Karaca <thisiseternity12@gmail.com>
2025-09-18 20:45:07 +0300

    Counting Sort 2

---
+++ Problem Solving/Sorting/counting_sort_2.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    freq = [0] * 100
+    for num in arr:
+        freq[num] += 1
+        
+    sorted_arr = []
+    for num in range(100):
+        sorted_arr.extend([num] * freq[num])
+        
+    return sorted_arr
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
